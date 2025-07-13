729bb1fde213ef1ce093821be8b25ab9e223a64b
Parshwa <parshwa282011@gmail.com>
2025-07-13 15:30:42 -0700

    finally merged all modding api

--- Assets/Scripts/Graphics/DrawSettings.cs
+++ Assets/Scripts/Graphics/DrawSettings.cs
@@ -13,6 +13,7 @@ namespace DLS.Graphics
 		public const float PinHeight1Bit = 0.185f;
 		public const float PinHeight4Bit = 0.3f;
 		public const float PinHeight8Bit = 0.43f;
+		public const float PinHeight16Bit = 0.6f; // 16 Bit pins are twice as high as 8 Bit pins
 		public const float PinRadius = PinHeight1Bit / 2;
 
 		public const FontType FontBold = FontType.JetbrainsMonoBold;
